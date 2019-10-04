38e8daf95f99f2e129f9f0bce114833e34ada1c6
박현빈 <sksms17456@naver.com>
2019-10-04 13:59:39 +0900

    df

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,7 @@ const { getUsers, getUser } = require('../controller');
 
 const router = new Router();
 
-router.get('/', (ctx) => ctx.body = '안녕하세요, 현빈정점입니ㅇㄴㅁㄹㄴㅇㄹ다!!되냐구.....');
+router.get('/', (ctx) => ctx.body = '안녕dsafasdf하세요, 현빈정점입니ㅇㄴㅁㄹㄴㅇㄹ다!!되냐구.....');
 router.get('/deploy', (ctx) => ctx.body = '무중단배포 성공');
 
 router.get('/users', getUsers);
