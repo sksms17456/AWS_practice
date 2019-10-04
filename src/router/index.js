c0342ef8a2c744c94cb56a321d81e6cca15b9768
박현빈 <sksms17456@naver.com>
2019-10-04 15:27:22 +0900

    asdf

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,7 @@ const { getUsers, getUser } = require('../controller');
 
 const router = new Router();
 
-router.get('/', (ctx) => ctx.body = '안녕dsafasdf하세요, 현빈정점입니ㅇㄴㅁㄹㄴㅇㄹ다!!되냐구.....');
+router.get('/', (ctx) => ctx.body = '근데 왜 3000번 포트로 그거안되냐 프록시');
 router.get('/deploy', (ctx) => ctx.body = '무중단배포 성공');
 
 router.get('/users', getUsers);
